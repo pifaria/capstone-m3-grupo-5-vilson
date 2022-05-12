2c23aec9e370dfc50a72c95033dcb1313c10961c
Raniery <raniery@pop-os.localdomain>
2022-05-12 14:43:08 -0300

    fix: tests removed from App.js

--- src/App.js
+++ src/App.js
@@ -12,40 +12,12 @@ import "react-toastify/dist/ReactToastify.css";
 Modal.setAppElement("#root");
 
 function App() {
-  const [token, setToken] = useState(null);
   const { modalIsOpen, closeModal, openModal } = useRegisterModal();
-
-  async function signupHandler() {
-    const resp = await requestApi.post("/register", {
-      email: "raniery@kenzie.com",
-      password: "1234",
-    });
-
-    setToken(resp.accessToken);
-  }
-
-  async function loginHandler() {
-    const resp = await requestApi.post("/login", {
-      email: "raniery@kenzie.com",
-      password: "1234",
-    });
-
-    setToken(resp.accessToken);
-  }
-
-  async function checkEvents() {
-    const eventList = await requestAuth("/events", token);
-
-    console.log(eventList);
-  }
 
   return (
     <div className="App">
       <GlobalStyle />
       <ToastContainer />
-      <button onClick={signupHandler}>Cadastrar</button>
-      <button onClick={loginHandler}>Login</button>
-      <button onClick={checkEvents}>Mostrar Eventos</button>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
