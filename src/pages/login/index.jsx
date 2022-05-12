58b4c29734249321264e4f61849433ec8a2ef1e1
tiagokp97 <tiagopesch@gmail.com>
2022-05-11 22:22:20 -0300

    feat: component input

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import Input from "../../components/input";
+
+export default function Login() {
+  const schema = yup.object().shape({
+    email: yup.string().email("Email inválido").required("Campo obrigatório!"),
+    password: yup
+      .string()
+      .min(8, "Mínimo de 8 dígitos")
+      .required("Campo obrigatório!"),
+  });
+  const history = useHistory();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmitFunction = (data) => {
+    console.log(data);
+    axios({
+      method: "post",
+      url: "https://clickfinder-json-server.herokuapp.com/login",
+      data,
+    })
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmitFunction)}>
+      <div>
+        <h1>Login</h1>
+      </div>
+
+      <Input
+        name="email"
+        error={errors.email?.message}
+        register={register}
+        placeholder="Digite aqui seu email"
+      />
+      <Input
+        register={register}
+        placeholder="Digite aqui seu senha"
+        name="password"
+        error={errors.password?.message}
+      />
+
+      <button>Entrar</button>
+
+      <p>Ainda não possui cadastro?</p>
+
+      <button>Cadastre-se</button>
+    </form>
+  );
+}
