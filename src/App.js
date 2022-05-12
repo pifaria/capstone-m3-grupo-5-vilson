ec877998d9cd59a7206064da4122bfff83347aa7
pifaria <luizfelipe.faria97@gmail.com>
2022-05-12 13:14:18 -0300

    delete: test button

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <button onClick={() => openModal()}>teste</button>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
