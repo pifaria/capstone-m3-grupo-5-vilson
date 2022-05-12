bdb6f6b67161a1b1eebf6aa65361704654a8c99f
Raniery <raniery@pop-os.localdomain>
2022-05-12 14:12:01 -0300

    BREAKING_CHANGES: Testes de requisição usando os services

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import { ToastContainer } from 'react-toastify'
-import GlobalStyle from './styles/global'
+import { ToastContainer } from "react-toastify";
+import GlobalStyle from "./styles/global";
 
-import 'react-toastify/dist/ReactToastify.css'
+import "react-toastify/dist/ReactToastify.css";
+import { requestApi, requestAuth } from "./services/API";
+import { useState } from "react";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  async function signupHandler() {
+    const resp = await requestApi.post("/register", {
+      email: "raniery@kenzie.com",
+      password: "1234",
+    });
+
+    setToken(resp.accessToken);
+  }
+
+  async function loginHandler() {
+    const resp = await requestApi.post("/login", {
+      email: "raniery@kenzie.com",
+      password: "1234",
+    });
+
+    setToken(resp.accessToken);
+  }
+
+  async function checkEvents() {
+    const eventList = await requestAuth("/events", token)
+
+    console.log(eventList);
+  }
+
   return (
     <div className="App">
-      <GlobalStyle/>
-      <ToastContainer/>
+      <GlobalStyle />
+      <ToastContainer />
+      <button onClick={signupHandler}>Cadastrar</button>
+      <button onClick={loginHandler}>Login</button>
+      <button onClick={checkEvents}>Mostrar Eventos</button>
     </div>
   );
 }
