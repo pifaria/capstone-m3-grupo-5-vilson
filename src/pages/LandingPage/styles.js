f06f45a1cb9f99761e5c9ce1cc0b814eef519310
Henrique Barros <henrique1_barros@hotmail.com>
2022-05-12 14:33:01 -0300

    feat: LandingPage component

---
+++ src/pages/LandingPage/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  height: 100vh;
+  background-color: var(--black);
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .text {
+    width: 217px;
+    font-family: Inter;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19.36px;
+    color: var(--vanilla);
+  }
+  p{
+    margin: 10px;
+  }
+`;
