e286a3e5f8911f136a2ae22930b44361a0e42f1d
Henrique Barros <henrique1_barros@hotmail.com>
2022-05-13 14:52:01 -0300

    feature: Dashboard Page

---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+import Header from "../../components/Header"
+
+const Dashboard = () => {
+  return <Container>
+      <Header placeholder={'Pesquise um ?'} />
+  </Container>;
+};
+
+export default Dashboard;
