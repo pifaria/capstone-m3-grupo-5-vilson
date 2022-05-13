479f35dc7af2110078b5dbe4bb56226154b32069
Eliane Discacciati <discacciati@gmail.com>
2022-05-13 18:26:04 -0300

    Feat: component event card

---
+++ src/components/EventCard/index.jsx
@@ -0,0 +1,12 @@
+import {Container} from "./styles.js"
+
+const EventCard = ({children}) =>{
+
+    return (
+        <Container >
+            {children}
+        </Container>
+    )
+};
+
+export default EventCard;
