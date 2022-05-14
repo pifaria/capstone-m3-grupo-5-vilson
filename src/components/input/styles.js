bb3ed97be7aac0dc4e130d3b5c6a256ecd6c4d46
tiagokp97 <tiagopesch@gmail.com>
2022-05-14 10:37:16 -0300

    styles: input more styles

--- src/components/input/styles.js
+++ src/components/input/styles.js
@@ -2,18 +2,17 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   input {
-        height: 35px;
-        background-color: transparent;
-        border: none;
-        border-bottom: 1px solid var(--orange);
-        caret-color: var(--orange);
-        padding-left: 5px;
+    height: 35px;
+    background-color: transparent;
+    border: none;
+    border-bottom: 1px solid var(--orange);
+    caret-color: var(--orange);
+    padding-left: 5px;
     color: var(--white);
 
     &:focus {
-            box-shadow: 0 0 0.7em var(--orange);
-            border-bottom: none;
-        }
+      box-shadow: 0 0 0.7em var(--orange);
+      border-bottom: none;
     }
+  }
 `;
-        
