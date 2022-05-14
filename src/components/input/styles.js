bce6c81b03f95c6d399ddc547e8e916f078c0bce
tiagokp97 <tiagopesch@gmail.com>
2022-05-14 10:35:39 -0300

    style: more styles to input

--- src/components/input/styles.js
+++ src/components/input/styles.js
@@ -1,19 +1,19 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
-    input{
-
+  input {
         height: 35px;
         background-color: transparent;
         border: none;
         border-bottom: 1px solid var(--orange);
         caret-color: var(--orange);
         padding-left: 5px;
+    color: var(--white);
 
-        &:focus{
+    &:focus {
             box-shadow: 0 0 0.7em var(--orange);
             border-bottom: none;
         }
     }
+`;
         
-`
