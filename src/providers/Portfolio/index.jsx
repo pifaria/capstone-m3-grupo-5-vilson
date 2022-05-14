f48cca8399f30db19b6674df7850aef0e2a5a7bf
Raniery <raniery@pop-os.localdomain>
2022-05-13 21:44:24 -0300

    feat: initial portfolio provider(test pending)

---
+++ src/providers/Portfolio/index.jsx
@@ -0,0 +1,56 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import requestApi from "../../services/API";
+import { userInfoContext } from "../userInfo";
+
+export const PortfolioContext = createContext();
+
+export const PortfolioProvider = ({ children }) => {
+  const { userInfo, saveUserInfo } = useContext(userInfoContext);
+  const [ portfolio, setPortfolio ] = useState(userInfo?.portfolio);
+
+  //Recebe como parametro um objeto photo com id e url
+  const addPhoto = async (photo) => {
+    const newPortfolio = userInfo.portfolio 
+      ? [...userInfo.portfolio, photo]
+      : [photo]
+
+    try {
+      const response = await requestApi.patchAuth(
+        `/users/${userInfo.id}`,
+        { portfolio:newPortfolio },
+        userInfo.accessToken
+      );
+
+      saveUserInfo(response.data)
+      setPortfolio(userInfo.portfolio);
+    } catch {
+      toast.error("Ops, ocorreu um erro ao enviar sua foto.");
+    }
+  };
+
+  const deletePhoto = async (id) => {
+    const newPortfolio = userInfo.portfolio.filter((photo) => {
+      return photo.id === id;
+    });
+
+    try {
+      const response = await requestApi.patchAuth(
+        `/users/${userInfo.id}`,
+        {portfolio: newPortfolio},
+        userInfo.accessToken
+      );
+
+      saveUserInfo(response.data)
+      setPortfolio(userInfo.portfolio);
+    } catch {
+      toast.error("Ops, ocorreu um erro ao enviar sua foto.");
+    }
+  };
+
+  return (
+    <PortfolioContext.Provider value={{portfolio, addPhoto, deletePhoto}}>
+      {children}
+    </PortfolioContext.Provider>
+  );
+};
