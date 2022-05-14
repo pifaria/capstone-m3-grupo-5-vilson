d5a59d660d97f9bba989cb81baad5cea8ce31cc3
Henrique Barros <henrique1_barros@hotmail.com>
2022-05-14 16:03:27 -0300

    feat: addEvent button

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,12 +1,15 @@
 import { Container } from "./styles";
 import Header from "../../components/Header";
+import Button from "../../components/Button";
 import { Redirect } from "react-router-dom";
 import { useContext } from "react";
 import { userInfoContext } from "../../providers/userInfo";
+import { useRegisterModal } from "../../providers/RegisterModal";
 import Budget from "../../components/Budget";
 
 const Dashboard = ({ authenticated, setAuthenticated }) => {
   const { userInfo } = useContext(userInfoContext);
+  const { openModal } = useRegisterModal();
 
   if (!authenticated) {
     return <Redirect to="/login" />;
@@ -18,6 +21,8 @@ const Dashboard = ({ authenticated, setAuthenticated }) => {
         placeholder={"Pesquise um ?"}
         setAuthenticated={setAuthenticated}
       />
+
+      <Button onClick={() => openModal()}> Adicionar evento</Button>
 
       {userInfo.type === "photographer" ? <Budget /> : null}
     </Container>
