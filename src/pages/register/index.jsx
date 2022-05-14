df4e130a0bd36c17e4c2f261a0863fc42277d51c
pifaria <luizfelipe.faria97@gmail.com>
2022-05-14 14:10:26 -0300

    feat: styles for register form

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -7,26 +7,39 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { FaEye } from "react-icons/fa";
 import requestApi from "../../services/API";
 import SelectStates from "../../components/SelectStates";
-import { AnimationContainer, Background, Container, Divider } from "./styles";
+import { AnimationContainer, Background, Container, Divider, InputContainer } from "./styles";
 import Button from "../../components/Button";
 import Modal from "../../components/Modal";
+import { RiArrowDropDownLine } from "react-icons/ri"
 
 export default function Register() {
   const schema = yup.object().shape({
     name: yup.string().required("Digite seu nome completo!"),
+    
     email: yup.string().email("Email inválido").required("Digite seu email!"),
+    
     password: yup
       .string()
-      .min(8, "Campo obrigatório")
+      .min(8, "Mínimo de 8 caracteres")
       .required("Campo obrigatório!")
       .matches(
         "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
         "Necessita caractere capital, minusculo, número e especial"
       ),
+
+    passwordConfirm: yup
+    .string()
+    .oneOf([yup.ref("password")], "Senhas não correspondem!")
+    .required("Campo obrigatório!"),
+    
     type: yup.string().required("Selecione um campo"),
+    
     avatar: yup.string(), //.required("Url da imagem inválido"),
+    
     states: yup.string(),
+    
     phone: yup.string(),
+    
     bio: yup.string(),
   });
 
@@ -66,94 +79,111 @@ export default function Register() {
       <Divider />
       <AnimationContainer>
         <form onSubmit={handleSubmit(onSubmitFunction)}>
-          <div>
-            <h1>Crie sua conta</h1>
-          </div>
-          <Input
-            register={register}
-            placeholder={errors.name?.message || "Nome"}
-            name="name"
-            error={errors.name?.message}
-          />
-          <Input
-            register={register}
-            placeholder={errors.email?.message || "Email"}
-            name="email"
-            error={errors.email?.message}
-          />
-          <Input
-            register={register}
-            label="Senha"
-            placeholder={errors.password?.message || "Senha"}
-            type="password"
-            name="password"
-            className="password"
-            icon={FaEye}
-            error={errors.password?.message}
-          />
-
-          <Input
-            register={register}
-            type="number"
-            placeholder={errors.phone?.message || "(xx) xxxxx-xxxx"}
-            name="number"
-            error={errors.phone?.message}
-          />
-          <div>
-            <SelectStates register={register} name="states" />
-          </div>
-          <select
-            name="type"
-            {...register("type")}
-            onChange={(e) =>
-              e.target.value === "photographer"
-                ? setShowInputs(true)
-                : setShowInputs(false)
-            }
-          >
-            <option value="" className="start-value">
-              Você é um cliente?
-            </option>
-            <option value="cliente">Sou cliente</option>
-            <option value="photographer">Sou fotógrafo</option>
-          </select>
-
-          {showInputs && (
             <div>
-              <div>
-                <select name="tags" {...register("tags")}>
-                  <option value="casamentos">Casamentos</option>
-                  <option value="ensaios">Ensaios</option>
-                  <option value="newborn">Newborn</option>
-                  <option value="eventos">Eventos</option>
-                  <option value="retratos">Retratos</option>
-                </select>
-              </div>
-              <div>
-                <textarea
-                  {...register("bio")}
-                  error
-                  placeholder="Fale um pouco sobre você..."
-                  name="bio"
-                />
-
-                <Input
-                  register={register}
-                  placeholder={errors.avatar?.message || "Link da sua foto"}
-                  name="avatar"
-                  error={errors.avatar?.message}
-                />
-              </div>
+              <h1>Crie sua conta</h1>
             </div>
-          )}
+          <InputContainer>
+            <Input
+              register={register}
+              placeholder={errors.name?.message || "Nome"}
+              name="name"
+              error={errors.name?.message}
+            />
+            <Input
+              register={register}
+              placeholder={errors.email?.message || "Email"}
+              name="email"
+              error={errors.email?.message}
+            />
+            <Input
+              register={register}
+              label="Senha"
+              placeholder={errors.password?.message || "Senha"}
+              type="password"
+              name="password"
+              className="password"
+              icon={FaEye}
+              error={errors.password?.message}
+            />
 
+            <Input
+              register={register}
+              label="Senha"
+              placeholder={errors.password?.message || "Confirmar senha"}
+              type="password"
+              name="passwordConfirm"
+              className="password"
+              icon={FaEye}
+              error={errors.password?.message}
+            />
+
+            <Input
+              register={register}
+              type="number"
+              placeholder={errors.phone?.message || "(xx) xxxxx-xxxx"}
+              name="number"
+              error={errors.phone?.message}
+            />
+            
+            <div className="optionsBox">
+              <RiArrowDropDownLine/>
+              <SelectStates register={register} name="states" />
+            </div>
+            
+            <div className="optionsBox">
+              <RiArrowDropDownLine/>
+              <select
+                name="type"
+                {...register("type")}
+                onChange={(e) =>
+                  e.target.value === "photographer"
+                    ? setShowInputs(true)
+                    : setShowInputs(false)
+                }
+                >
+                <option value="" className="start-value">
+                  Você é um cliente?
+                </option>
+                <option value="cliente">Sou cliente</option>
+                <option value="photographer">Sou fotógrafo</option>
+              </select>
+            </div>
+
+            {showInputs && (
+              <div>
+                <div className="optionsBox">
+                  <RiArrowDropDownLine/>
+                  <select name="tags" {...register("tags")}>
+                    <option value="casamentos">Casamentos</option>
+                    <option value="ensaios">Ensaios</option>
+                    <option value="newborn">Newborn</option>
+                    <option value="eventos">Eventos</option>
+                    <option value="retratos">Retratos</option>
+                  </select>
+                </div>
+                <div>
+                  <textarea
+                    {...register("bio")}
+                    error
+                    placeholder="Fale um pouco sobre você..."
+                    name="bio"
+                  />
+
+                  <Input
+                    register={register}
+                    placeholder={errors.avatar?.message || "Link da sua foto"}
+                    name="avatar"
+                    error={errors.avatar?.message}
+                  />
+                </div>
+              </div>
+            )}
+          </InputContainer>
           <div className="container-button">
             <Button>Cadastre-se</Button>
 
-            <p>Já possui uma conta?</p>
-
             <p>
-              Faça seu <span onClick={() => history.push("/login")}>login</span>
+              Já possui uma conta? Faça seu <span onClick={() => history.push("/login")}>login</span>
             </p>
           </div>
         </form>
