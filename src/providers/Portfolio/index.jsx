cbdc9b725532d20fa1844b79185656af7e7f0025
Raniery <raniery@pop-os.localdomain>
2022-05-14 21:57:24 -0300

    fix: portfolio sending correct user info and updating properly whenever a change is made

--- src/providers/Portfolio/index.jsx
+++ src/providers/Portfolio/index.jsx
@@ -1,32 +1,38 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import requestApi from "../../services/API";
 import { userInfoContext } from "../userInfo";
-import { useIDGenerator } from "../IDGenerator"
+import { useIDGenerator } from "../IDGenerator";
 
 const PortfolioContext = createContext();
 
 export const PortfolioProvider = ({ children }) => {
   const { userInfo, saveUserInfo } = useContext(userInfoContext);
   const { generateId } = useIDGenerator();
-  const [ portfolio, setPortfolio ] = useState(userInfo?.portfolio);
+  const [portfolio, setPortfolio] = useState();
+
+  useEffect(() => {
+    setPortfolio(userInfo.portfolio ? userInfo.portfolio : [])
+  }, [userInfo])
 
   const addPhoto = async (url) => {
-    const photoId = generateId();
-    const photo   = {id: photoId, url}
+    const {accessToken, id: userId} = userInfo;
 
-    const newPortfolio = userInfo.portfolio 
-      ? [...userInfo.portfolio, photo]
-      : [photo]
+    const photoId = generateId();
+    const photo = { id: photoId, url };
+
+    console.log(portfolio)
+
+    const newPortfolio = [...portfolio, photo];
 
     try {
       const response = await requestApi.patchAuth(
-        `/users/${userInfo.id}`,
-        { portfolio:newPortfolio },
-        userInfo.accessToken
+        `/users/${userId}`,
+        { portfolio: newPortfolio },
+        accessToken
       );
 
-      saveUserInfo(response.data)
+      saveUserInfo({user: {...response.data}, accessToken});
       setPortfolio(userInfo.portfolio);
     } catch {
       toast.error("Ops, ocorreu um erro ao enviar sua foto.");
@@ -34,18 +40,20 @@ export const PortfolioProvider = ({ children }) => {
   };
 
   const deletePhoto = async (id) => {
-    const newPortfolio = userInfo.portfolio.filter((photo) => {
-      return photo.id === id;
+    const {id :userId, accessToken} = userInfo;
+
+    const newPortfolio = portfolio.filter((photo) => {
+      return photo.id !== id;
     });
 
     try {
       const response = await requestApi.patchAuth(
-        `/users/${userInfo.id}`,
-        {portfolio: newPortfolio},
+        `/users/${userId}`,
+        { portfolio: newPortfolio },
         userInfo.accessToken
       );
 
-      saveUserInfo(response.data)
+      saveUserInfo({user: {...response.data}, accessToken});
       setPortfolio(userInfo.portfolio);
     } catch {
       toast.error("Ops, ocorreu um erro ao enviar sua foto.");
@@ -53,7 +61,7 @@ export const PortfolioProvider = ({ children }) => {
   };
 
   return (
-    <PortfolioContext.Provider value={{portfolio, addPhoto, deletePhoto}}>
+    <PortfolioContext.Provider value={{ portfolio, addPhoto, deletePhoto }}>
       {children}
     </PortfolioContext.Provider>
   );
