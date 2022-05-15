60989f97fac1865fc46ee813ecf32e64f7d9d9f0
Raniery <raniery@pop-os.localdomain>
2022-05-14 21:52:18 -0300

    feat: Modal for adding a new picture

--- src/components/PhotoGalery/index.jsx
+++ src/components/PhotoGalery/index.jsx
@@ -1,24 +1,27 @@
-import { useContext } from "react";
-import { PortfolioContext } from "../../providers/Portfolio";
-import { Container, Content } from "./styles";
+import { useState } from "react";
+import Modal from "react-modal/lib/components/Modal";
+import { Container, Content, ContentList } from "./styles";
 
-function PhotoGalery() {
-  const { portfolio, addPhoto, deletePhoto } = useContext(PortfolioContext);
-
-  function onAddPhoto(url) {
-    addPhoto(url);
-  }
+function PhotoGalery({photos, addPhoto, deletePhoto}) {
+  const [openModal, setOpenModal] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
   function onDeletePhoto(id) {
     deletePhoto(id);
   }
 
+  function onSendPhoto(event) {
+    event.preventDefault();
+
+    addPhoto(inputValue);
+  }
+
   return (
     <Container>
       <Content>
-        <button onClick={() => onAddPhoto()}>+</button>
+        <button onClick={() => setOpenModal(true)}>+</button>
         <ContentList>
-          {portfolio.map(({id, url}) => {
+          {photos.map(({ id, url }) => {
             return (
               <li key={id}>
                 <img src={url} alt="photo" />
@@ -27,7 +30,16 @@ function PhotoGalery() {
             );
           })}
         </ContentList>
+        <Modal isOpen={openModal}>
+          <form onSubmit={onSendPhoto}>
+            <input onChange={({ target: { value } }) => setInputValue(value)} />
+            <button type="submit">Enviar</button>
+          </form>
+          <button onClick={() => setOpenModal(false)}>X</button>
+        </Modal>
       </Content>
     </Container>
   );
 }
+
+export default PhotoGalery;
