312bde934f11b6d7659959cee2fba6d5cdaa4fd2
tiagokp97 <tiagopesch@gmail.com>
2022-05-15 16:27:54 -0300

    styles: eventCard styles

--- src/components/EventCard/styles.js
+++ src/components/EventCard/styles.js
@@ -1,4 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 300px;
+  height: 315px;
+  background-color: var(--orange);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  text-align: center;
+
+  button {
+    align-self: center;
+  }
 `;
