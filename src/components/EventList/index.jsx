93f083cd35fb0d3d9c1f572b0316712af4e3b779
tiagokp97 <tiagopesch@gmail.com>
2022-05-15 16:27:07 -0300

    fix: eventList not rendering

--- src/components/EventList/index.jsx
+++ src/components/EventList/index.jsx
@@ -1,50 +1,48 @@
-import {Container} from "./styles.js"
-import { useEventList } from "../../providers/EventList/index.jsx";
+import { Container } from "./styles.js";
+import {
+  EventListContext,
+  useEventList,
+} from "../../providers/EventList/index.jsx";
 import EventCard from "../EventCard/index.jsx";
 import Button from "../Button/index";
 import { userInfoContext } from "../../providers/userInfo/index.jsx";
 import { useContext } from "react";
 
-
-const EventList = () =>{
-
-    const {eventList} = useEventList();
-    const {userInfo} = useContext(userInfoContext);
-
-    return (
-        <Container >
-            {eventList.length>0 && 
-                eventList.map( (element, index)=>
-                    <li key={index}> 
-                        <EventCard> 
-                            {userInfo.type === "client" ?
-                                <>
-                                <p> Tique </p>            
-                                <Button>Ver Evento</Button>
-                                </>
-                            :
-                                <>
-                                <p> Tique </p> 
-                                <h3>{element.userInfos.name}</h3>   
-                                <h4>{element.userInfos.number}</h4>
-                                <h4>{element.userInfos.email}</h4>
-                                </>
-                            } 
-                            <h3>{element.tags}</h3>
-                            <h3>{element.title}</h3>
-                            <h4>{element.description}</h4>
-                            <h4>{element.date}</h4> 
-                            <h4>{element.local}</h4>
-                            <h4>{element.state}</h4>                   
-                            <Button beigeSchema>Excluir</Button>
-                        </EventCard> 
-                    </li>
-
-                )
-            
-            }
-        </Container>
-    )
+const EventList = () => {
+  const { userInfo } = useContext(userInfoContext);
+  const { eventsList } = useContext(EventListContext);
+  console.log(eventsList);
+  return (
+    <Container>
+      {eventsList.length > 0 &&
+        eventsList.map((element, index) => (
+          <li key={index}>
+            <EventCard>
+              {userInfo.type === "client" ? (
+                <>
+                  <p> Tique </p>
+                  <Button>Ver Evento</Button>
+                </>
+              ) : (
+                <>
+                  <p> Tique </p>
+                  <h3>{element.userInfos.name}</h3>
+                  <h4>{element.userInfos.number}</h4>
+                  <h4>{element.userInfos.email}</h4>
+                </>
+              )}
+              <h3>{element.tags}</h3>
+              <h3>{element.title}</h3>
+              <h4>{element.description}</h4>
+              <h4>{element.date}</h4>
+              <h4>{element.local}</h4>
+              <h4>{element.state}</h4>
+              <Button beigeSchema>Excluir</Button>
+            </EventCard>
+          </li>
+        ))}
+    </Container>
+  );
 };
 
 export default EventList;
