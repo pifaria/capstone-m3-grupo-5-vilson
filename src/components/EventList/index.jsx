599b0af84bf0eab190d5e9ae0964247fe14ac629
Henrique Barros <henrique1_barros@hotmail.com>
2022-05-16 00:36:47 -0300

    fix: eventList refatored

--- src/components/EventList/index.jsx
+++ src/components/EventList/index.jsx
@@ -4,42 +4,16 @@ import {
   useEventList,
 } from "../../providers/EventList/index.jsx";
 import EventCard from "../EventCard/index.jsx";
-import Button from "../Button/index";
-import { userInfoContext } from "../../providers/userInfo/index.jsx";
 import { useContext } from "react";
 
 const EventList = () => {
-  const { userInfo } = useContext(userInfoContext);
   const { eventsList } = useContext(EventListContext);
-  console.log(eventsList);
+
   return (
     <Container>
       {eventsList.length > 0 &&
-        eventsList.map((element, index) => (
-          <li key={index}>
-            <EventCard>
-              {userInfo.type === "cliente" ? (
-                <>
-                  <p> Tique </p>
-                  <Button>Ver Evento</Button>
-                </>
-              ) : (
-                <>
-                  <p> Tique </p>
-                  <h3>{element.userInfos.name}</h3>
-                  <h4>{element.userInfos.number}</h4>
-                  <h4>{element.userInfos.email}</h4>
-                </>
-              )}
-              <h3>{element.tags}</h3>
-              <h3>{element.title}</h3>
-              <h4>{element.description}</h4>
-              <h4>{element.date}</h4>
-              <h4>{element.local}</h4>
-              <h4>{element.state}</h4>
-              <Button beigeSchema>Excluir</Button>
-            </EventCard>
-          </li>
+        eventsList.map((event) => (
+            <EventCard event={event} />
         ))}
     </Container>
   );
