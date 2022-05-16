db3b9a065b20e6d8057c9bbf3160f54c5d0afbdd
Henrique Barros <henrique1_barros@hotmail.com>
2022-05-16 00:37:55 -0300

    feat: photographer name

--- src/components/PhotographerCard/index.jsx
+++ src/components/PhotographerCard/index.jsx
@@ -2,13 +2,13 @@ import Button from "../Button";
 import { Container, Content } from "./styles";
 import { HiOutlineCamera } from "react-icons/hi";
 
-const PhotographerCard = () => {
+const PhotographerCard = ({info}) => {
 
   return (
     <Container>
       <Content>
           <div className="avatar"></div>
-          <h4>Jennis Wilson</h4>
+          <h4>{info.name}</h4>
           <div className="profile"><HiOutlineCamera className="icon" /><h3>Ver perfil</h3></div>
         <div className="buttons">
           <Button >Solicitar</Button>
