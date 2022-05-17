337874bf2194b11a77ef4c19e6384c3d35fdeb93
tiagokp97 <tiagopesch@gmail.com>
2022-05-16 21:15:08 -0300

    feat: addded more styles

--- src/components/PhotoGalery/index.jsx
+++ src/components/PhotoGalery/index.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import Modal from "react-modal/lib/components/Modal";
-import { Container, Content, ContentList } from "./styles";
+import { Container, ContainerButton, Content, ContentList } from "./styles";
 
-import { usePortfolio } from "../../providers/Portfolio"
+import { usePortfolio } from "../../providers/Portfolio";
 
 function PhotoGalery() {
   const [openModal, setOpenModal] = useState(false);
   const [inputValue, setInputValue] = useState("");
 
-  const { portfolio, addPhoto, deletePhoto } = usePortfolio()
+  const { portfolio, addPhoto, deletePhoto } = usePortfolio();
 
   function onDeletePhoto(id) {
     deletePhoto(id);
@@ -22,17 +22,22 @@ function PhotoGalery() {
 
   return (
     <Container>
-      <Content>
+      <ContainerButton>
         <button onClick={() => setOpenModal(true)}>+</button>
+        <div className="orange-bar"></div>
+        <p>Portfólio</p>
+      </ContainerButton>
+      <Content>
         <ContentList>
-          {portfolio && portfolio.map(({ id, url }) => {
-            return (
-              <li key={id}>
-                <img src={url} alt="photo" />
-                <button onClick={() => onDeletePhoto(id)}>x</button>
-              </li>
-            );
-          })}
+          {portfolio &&
+            portfolio.map(({ id, url }) => {
+              return (
+                <li key={id}>
+                  <img src={url} alt="photo" />
+                  <button onClick={() => onDeletePhoto(id)}>x</button>
+                </li>
+              );
+            })}
         </ContentList>
 
         <Modal isOpen={openModal}>
