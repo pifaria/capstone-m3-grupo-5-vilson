0bcbf1a623333effb31bce3975c74d28b68332a8
Eliane Discacciati <discacciati@gmail.com>
2022-05-16 21:27:11 -0300

    fix: component PhotographerList and page Event

--- src/components/PhotographerList/index.jsx
+++ src/components/PhotographerList/index.jsx
@@ -2,13 +2,15 @@ import { Container } from "./styles.js";
 import PhotographerCard from "../PhotographerCard/index.jsx";
 import {usePhotographerList} from "../../providers/PhotographerList/index";
 
-const PhotographersList = () => {
-    const { photographersList } = usePhotographerList();
+const PhotographerList = () => {
+    const { photographerList } = usePhotographerList();
+
+    console.log(photographerList)
 
     return(
         <Container>        
-            {photographersList.length > 0 &&
-                photographersList.map((photographer, index) => {
+            {photographerList.length > 0 &&
+                photographerList.map((photographer, index) => {
                     return <PhotographerCard key={index} info={photographer} />;
                 })
             }
@@ -16,4 +18,4 @@ const PhotographersList = () => {
   )
 }
 
-export default PhotographersList;
+export default PhotographerList;
