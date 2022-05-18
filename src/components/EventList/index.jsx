055ee8d4d15332132db561d5c0ebb3f858673862
Eliane Discacciati <discacciati@gmail.com>
2022-05-17 22:50:45 -0300

    Fix:dashboard button add events

--- src/components/EventList/index.jsx
+++ src/components/EventList/index.jsx
@@ -1,10 +1,6 @@
 import { Container } from "./styles.js";
-import {
-  EventListContext,
-  useEventList,
-} from "../../providers/EventList/index.jsx";
+import {useEventList} from "../../providers/EventList/index.jsx";
 import EventCard from "../EventCard/index.jsx";
-import { useContext } from "react";
 
 const EventList = () => {
   const { eventsList } = useEventList();
