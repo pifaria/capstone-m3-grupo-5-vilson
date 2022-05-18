cfa7565b53aca886f931e3fab6feb0e862402908
Raniery <raniery@pop-os.localdomain>
2022-05-17 23:29:28 -0300

    fix: add photo modal now closes on success, success toast added, photo galery updates automatically

--- src/components/EventList/index.jsx
+++ src/components/EventList/index.jsx
@@ -1,10 +1,6 @@
 import { Container } from "./styles.js";
-import {
-  EventListContext,
-  useEventList,
-} from "../../providers/EventList/index.jsx";
+import { useEventList } from "../../providers/EventList/index.jsx";
 import EventCard from "../EventCard/index.jsx";
-import { useContext } from "react";
 
 const EventList = () => {
   const { eventsList } = useEventList();
@@ -12,9 +8,7 @@ const EventList = () => {
   return (
     <Container>
       {eventsList.length > 0 &&
-        eventsList.map((event) => (
-            <EventCard key={event.id} event={event} />
-        ))}
+        eventsList.map((event) => <EventCard key={event.id} event={event} />)}
     </Container>
   );
 };
--- src/components/PhotoGalery/index.jsx
+++ src/components/PhotoGalery/index.jsx
@@ -2,10 +2,10 @@ import { useState, useEffect } from "react";
 import Modal from "react-modal/lib/components/Modal";
 import { Container, ContainerButton, Content, ContentList } from "./styles";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-import axios from "axios";
 import { usePortfolio } from "../../providers/Portfolio";
 import { useUserInfo } from "../../providers/userInfo";
 import PhotoModal from "../PhotoModal";
+import requestApi from "../../services/API";
 
 function PhotoGalery() {
   const params = useParams();
@@ -13,19 +13,15 @@ function PhotoGalery() {
 
   const [openModal, setOpenModal] = useState(false);
   const [inputValue, setInputValue] = useState("");
-  const { addPhoto, deletePhoto } = usePortfolio();
+  const { portfolio, addPhoto, deletePhoto, getPortfolio } = usePortfolio();
 
   const { userInfo } = useUserInfo();
   const [maximizeEvent, setMaximizeEvent] = useState(false);
   const [newUrl, setNewUrl] = useState("");
 
   useEffect(() => {
-    axios
-      .get(
-        `https://clickfinder-json-server.herokuapp.com/users/${parseInt(
-          params.id
-        )}`
-      )
+    requestApi
+      .get(`/users/${parseInt(params.id)}`)
       .then((response) => {
         setProfileUser(response.data);
       })
@@ -39,7 +35,10 @@ function PhotoGalery() {
   function onSendPhoto(event) {
     event.preventDefault();
 
-    addPhoto(inputValue);
+    addPhoto(inputValue, ()=> {
+      setOpenModal(false);
+    });
+    getPortfolio();
   }
 
   const bigPicture = (imgUrl) => {
@@ -73,8 +72,8 @@ function PhotoGalery() {
           <ContentList>
             {userInfo.id === profileUser.id ? (
               <>
-                {profileUser.portfolio &&
-                  profileUser.portfolio.map(({ id, url }) => {
+                {portfolio &&
+                  portfolio.map(({ id, url }) => {
                     return (
                       <li key={id}>
                         <img src={url} alt="photo" />
@@ -88,58 +87,13 @@ function PhotoGalery() {
                 {profileUser.portfolio &&
                   profileUser.portfolio.map(({ id, url }) => {
                     return (
-                      <>
-                        <li key={id}>
-                          <img
-                            src={url}
-                            alt="event"
-                            onClick={() => bigPicture(url)}
-                          />
-                        </li>
-
-                        <li>
-                          <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Taj_Mahal_N-UP-A28-a.jpg/1200px-Taj_Mahal_N-UP-A28-a.jpg"
-                            alt="taj"
-                            onClick={() =>
-                              bigPicture(
-                                "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Taj_Mahal_N-UP-A28-a.jpg/1200px-Taj_Mahal_N-UP-A28-a.jpg"
-                              )
-                            }
-                          />
-                        </li>
-
-                        <li>
-                          <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Taj_Mahal_N-UP-A28-a.jpg/1200px-Taj_Mahal_N-UP-A28-a.jpg"
-                            alt="taj"
-                          />
-                        </li>
-                        <li>
-                          <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Taj_Mahal_N-UP-A28-a.jpg/1200px-Taj_Mahal_N-UP-A28-a.jpg"
-                            alt="taj"
-                          />
-                        </li>
-                        <li>
-                          <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Taj_Mahal_N-UP-A28-a.jpg/1200px-Taj_Mahal_N-UP-A28-a.jpg"
-                            alt="taj"
-                          />
-                        </li>
-                        <li>
-                          <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Taj_Mahal_N-UP-A28-a.jpg/1200px-Taj_Mahal_N-UP-A28-a.jpg"
-                            alt="taj"
-                          />
-                        </li>
-                        <li>
-                          <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Taj_Mahal_N-UP-A28-a.jpg/1200px-Taj_Mahal_N-UP-A28-a.jpg"
-                            alt="taj"
-                          />
-                        </li>
-                      </>
+                      <li key={id}>
+                        <img
+                          src={url}
+                          alt="event"
+                          onClick={() => bigPicture(url)}
+                        />
+                      </li>
                     );
                   })}
               </>
