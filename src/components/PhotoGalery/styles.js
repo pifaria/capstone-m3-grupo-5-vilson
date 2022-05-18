695061920c1ad6a1a1d80ec090d647761f83b8ae
tiagokp97 <tiagopesch@gmail.com>
2022-05-17 21:13:26 -0300

    styles: created more styles to PhotoGalery

--- src/components/PhotoGalery/styles.js
+++ src/components/PhotoGalery/styles.js
@@ -5,6 +5,10 @@ export const Container = styled.div`
   flex-direction: column;
   row-gap: 20px;
   margin-top: 20px;
+
+  @media (min-width: 1024px) {
+    margin-left: 10%;
+  }
 `;
 
 export const ContainerButton = styled.div`
@@ -31,13 +35,23 @@ export const ContainerButton = styled.div`
   p {
     color: var(--orange);
   }
+
+  @media (min-width: 1024px) {
+    .orange-bar {
+      width: 480px;
+    }
+
+    p {
+      font-size: 40px;
+    }
+  }
 `;
 
 export const Content = styled.div`
   display: flex;
   justify-content: center;
-  overflow-x: hidden;
   height: 200px;
+  overflow-y: scroll;
   ::-webkit-scrollbar {
     width: 3px;
   }
@@ -51,22 +65,51 @@ export const Content = styled.div`
     background: var(--orange);
     border-radius: 10px;
   }
+
+  @media (min-width: 1024px) {
+    overflow: initial;
+    height: 600px;
+  }
 `;
 
 export const ContentList = styled.ul`
   list-style: none;
   display: flex;
-  width: 280px;
-  flex-wrap: wrap;
   gap: 20px;
+  flex-wrap: wrap;
 
   li {
     display: flex;
-    flex-direction: column;
   }
 
   img {
     height: 80px;
     width: 80px;
+    border-radius: 4px;
+  }
+
+  @media (min-width: 1024px) {
+    max-height: 600px;
+    overflow-y: scroll;
+    ::-webkit-scrollbar {
+      width: 3px;
+    }
+
+    ::-webkit-scrollbar-track {
+      box-shadow: inset 0 0 5px var(--beige);
+      border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: var(--orange);
+      border-radius: 10px;
+    }
+    li {
+      flex-direction: row;
+    }
+    img {
+      width: 230px;
+      height: 230px;
+    }
   }
 `;
