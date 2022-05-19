3808d959bd084bd56ce53de120fb7ee4b69f662e
Raniery <raniery@pop-os.localdomain>
2022-05-18 23:34:34 -0300

    fix: EventList component now shows an apropriate message when empty

--- src/components/EventList/index.jsx
+++ src/components/EventList/index.jsx
@@ -1,5 +1,5 @@
 import { Container } from "./styles.js";
-import {useEventList} from "../../providers/EventList/index.jsx";
+import { useEventList } from "../../providers/EventList/index.jsx";
 import EventCard from "../EventCard/index.jsx";
 import { useState } from "react";
 import { useUserInfo } from "../../providers/userInfo/index.jsx";
@@ -9,7 +9,7 @@ const EventList = () => {
   const { eventsList, getEventList } = useEventList();
 
   useState(() => {
-    if(!userInfo) {
+    if (!userInfo) {
       return;
     }
 
@@ -18,6 +18,13 @@ const EventList = () => {
 
   return (
     <Container>
+      {eventsList.length === 0 && (
+        <span>
+          {userInfo?.type === "cliente" || userInfo?.type === "client"
+            ? "Você ainda não cadastrou nenhum evento."
+            : "Ainda não há eventos disponíveis para você."}
+        </span>
+      )}
       {eventsList.length > 0 &&
         eventsList.map((event) => <EventCard key={event.id} event={event} />)}
     </Container>
